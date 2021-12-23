6c32614dab844c00872f6785c2c2fef2963c4f2a
Fatih <95652578+fatihqaz@users.noreply.github.com>
2021-12-23 17:26:02 +0300

    .gitignore edit

--- 04.Day-Basic Arrays/03-Student Attendance/script.js
+++ 04.Day-Basic Arrays/03-Student Attendance/script.js
@@ -7,18 +7,17 @@ const attendances = [
   ["Nina", 10]
 ]
 
+console.log(attendances)
+
 attendances.push(["Ali", 11], ["Veli", 7], ["Deli", 9])
 
 console.log(attendances)
 
-console.log(attendances.indexOf([2]["Elamin"]))
 
-console.log(attendances[3][1])
-
-attendances[3][1]=8
-
+attendances[2][1]=8
 
 console.log(`Bugün ${attendances.length} öğrenci sınıfa geldi. İlk öğencinin adı ${attendances[0][0]} ve son öğrencinin adı ${attendances[attendances.length-1][0]}` )
+
 
 let yas= ((attendances[0][1]+attendances[1][1]+attendances[2][1]+attendances[3][1]+attendances[4][1]+attendances[5][1]+attendances[6][1]+attendances[7][1]+attendances[8][1])/attendances.length).toFixed(2)
 
