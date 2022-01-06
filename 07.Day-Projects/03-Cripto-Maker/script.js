36c8a2a8cc83b16b5176965127b3590684d1b5a0
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-01-06 03:00:14 +0300

    proje değişiklik

--- 07.Day-Projects/03-Cripto-Maker/script.js
+++ 07.Day-Projects/03-Cripto-Maker/script.js
@@ -1,3 +1,4 @@
+/*
 const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const alpha = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
 const spec = ["+", ".", "*", "/", "$", "&", "%"]
@@ -33,16 +34,13 @@ console.log(crypto(10))
 //STEP 2
 const special = "* $ & %"
 
-let specArr = special.split(" ")
+
 let newCryp = crypto(20).split("")
 
 
-
-let cleanArr = newCryp.filter(item => item !== "*" && item !== "$"&& item !== "&"&& item !== "%")
+let cleanArr = newCryp.filter(item => item !== "*" && item !== "$" && item !== "&" && item !== "%")
 
 console.log(cleanArr.join(""))
-
-
 
 
 
@@ -54,3 +52,62 @@ console.log(cleanArr.join(""))
 
 //STEP 4
 givenUnwanted = "3 , * , j , q , z"
+
+*/
+
+
+const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const alpha = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+const alpha1 = alpha.split(" ")
+
+const special = "* $ & %"
+const special1 = special.split(" ");
+
+
+const newArray = [...numeric, ...alpha1, ...special1];
+
+
+const givenUnwanted = "-*/"
+const arrayGiven = givenUnwanted.split("");
+
+let sizepaswort = 10
+
+
+function passwort(arr, size) {
+
+    let newArray = [];
+
+    for (let i = 0; i < size; i++) {
+
+        let sonuc = arr[Math.floor(Math.random() * arr.length)]
+
+        if (sonuc.toFixed && (i < size - 1)) {
+
+            newArray += (sonuc + "-")
+
+        } else {
+
+            newArray += (sonuc)
+        }
+    }
+    return newArray
+};
+
+function kimlik(par) {
+
+    let bos = [];
+
+    for (let i = 0; i < newArray.length; i++) {
+
+        for (let k = 0; k < par.length; k++) {
+
+            if (newArray[i] == par[k]) {
+
+                newArray.splice(i, 1)
+            }
+        };
+        bos.push(newArray[i])
+    }
+    return bos;
+};
+console.log(passwort(kimlik(arrayGiven), sizepaswort));
